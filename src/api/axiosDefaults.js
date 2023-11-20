4ed923509a5ea8ac048de646b211a8a1d9c90b72
PjHurtig <>
2023-11-20 16:18:03 +0000

    add logged in/out functionality and avatar component

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://hbc-api-pj-9ce30abdc101.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
