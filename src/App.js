321da0814aa00ed09a43829ad8b0f7a4bff5c015
PjHurtig <>
2023-11-22 20:53:22 +0000

    add post and postpage

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
 
 function App() {
   return (
         <div className={styles.App}>
           <NavBar />
-          <Container className={styles.Main}> 
+          <Container className={styles.Main}>
             <Switch>
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+              <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
