da966e9056f491246467132548246ae83069816f
PjHurtig <>
2023-11-27 20:58:36 +0000

    add onSelect prop and handle function to calendar for linking to eventpages

--- src/pages/calendars/CalendarPage.js
+++ src/pages/calendars/CalendarPage.js
@@ -4,22 +4,38 @@ import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 const localizer = momentLocalizer(moment)
 
 const CalendarPage = () => {
   const [events, setEvents] = useState([]);
+  const history = useHistory();
+
+  const categoryColors = {
+    bike: "blue",
+    hiking: "green",
+    climbing: "red",
+    other: "grey",
+  };
+
+  const handleEventSelect = (event) => {
+    history.push(`/events/${event.id}`);
+  };
+  
 
   useEffect(() => {
     const fetchEvents = async () => {
       try {
         const response = await axios.get("/events/");
         const formattedEvents = response.data.results.map(event => ({
+          id: event.id,
           title: event.title,
           start: new Date(event.start_time),
           end: new Date(event.start_time),
           allDay: false,
+          category: event.category,
 
         }));
         setEvents(formattedEvents);
@@ -32,6 +48,19 @@ const CalendarPage = () => {
   }, []);
 
 
+  const getEventStyle = (event) => {
+    let backgroundColor = categoryColors[event.category] || categoryColors.other;
+    let style = {
+      backgroundColor: backgroundColor,
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+      border: 'none',
+      display: 'block',
+    };
+    return { style };
+  };
+
   return (
     <div>
       <Calendar
@@ -40,6 +69,8 @@ const CalendarPage = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        eventPropGetter={getEventStyle}
+        onSelectEvent={handleEventSelect}
       />
     </div>
   );
