243b4f45531ff74d4ec172d86b610409b0735106
PjHurtig <>
2023-11-28 23:40:37 +0000

    update event.js layout

--- src/pages/events/Event.js
+++ src/pages/events/Event.js
@@ -1,7 +1,7 @@
 import React from "react";
-import styles from "../../styles/Event.module.css";
+import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
-import { Card, Col, Media } from "react-bootstrap";
+import { Card, Media } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
@@ -18,6 +18,8 @@ const Event = (props) => {
     image,
     eventPage,
     start_time,
+    updated_at,
+    category,
   } = props;
 
 
@@ -39,40 +41,41 @@ const Event = (props) => {
   };
 
   return (
-    <Col lg={8} className="mx-auto mb-4">
-      <Card className={styles.Event}>
-        <Card.Body>
-          <Media className="align-items-center justify-content-between">
-            <Link to={`/profiles/${profile_id}`}>
-              <Avatar src={profile_image} height={55} />
-              {owner}
-            </Link>
-            {title && <Card.Title className="text-center">{title}</Card.Title>}
-            <div className="d-flex align-items-center">
-              {is_owner && eventPage && (
-                <MoreDropdown
-                  handleEdit={handleEdit}
-                  handleDelete={handleDelete}
-                />
-              )}
-            </div>
-          </Media>
-        </Card.Body>
-        
-        <Card.Body>
-          {description && <Card.Text>{description}</Card.Text>}
-          <div className={styles.PostBar}>
-          <Link to={`/events/${id}`}>
-          <Card.Img src={image} alt={title} />
-        </Link>
-            <Link to={`/events/${id}`}>
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && eventPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`/events/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+      {title && <Card.Title className="text-center">{title} - {category}</Card.Title>}
+        {description && <Card.Text>{description}</Card.Text>}
+        <div className={styles.PostBar}>
+        <Link to={`/events/${id}`}>
               <i className="far fa-calendar" />
             </Link>
-          </div>
           <span>{start_time}</span>
-        </Card.Body>
-      </Card>
-    </Col>
+          <Link to={`/events/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
