97b15b0a6ed2d15dd3024df05fc43f477eceebf4
PjHurtig <>
2023-11-29 14:51:44 +0000

    add styles and placeholder to eventpage.js

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Event from "./Event";
@@ -28,10 +31,13 @@ function EventPage() {
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2">
-        <Container>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <Container className={appStyles.Content}>
         <Event {...event.results[0]} setEvents={setEvent} eventPage />
         </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
       </Col>
     </Row>
   );
